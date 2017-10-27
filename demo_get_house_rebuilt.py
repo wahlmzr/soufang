0ab4b5f145c2287196130e63945ef0f20cc2573c
wahlmzr <wahlmzr@163.com>
2017-10-28 01:23:40 +0800

    demo_get_house_rebuilt
    把网页解析了一下

--- demo_get_house_rebuilt.py
+++ demo_get_house_rebuilt.py
@@ -33,14 +33,87 @@ headers = {
 i = 1
 url = 'http://esf.sh.fang.com/house-a019/i3%s'%(str(i))
 html = requests.get(url,headers=headers).text
-f = open('demo.html','w',encoding='utf-8')
-f.write(html)
-f.close()
 
 #dl
 soup =BeautifulSoup(html,'lxml')
 
 import re
 for item in soup.find_all("dl",id = re.compile("list_D03_")) :
-    print (item)
+#    print (item)
     print ('--------------')
+    id = item['id']
+    href = item.a['href']
+    print (id , href)
+    
+
+href = '/chushou/3_306950858.htm'
+root_url = 'http://esf.sh.fang.com'
+html = requests.get(root_url+href,headers = headers).text
+f = open('demo_detail.html','w',encoding = 'utf8')
+f.write(html)
+f.close()            
+
+
+soup =BeautifulSoup(html,'lxml')
+
+total = soup.find('div',class_ = 'trl-item sty1').i.text
+
+a = soup.find('div',class_ = 'bqian clearfix')
+    
+q = []
+for item in a.find_all('span'):
+    q.append(item.text)
+
+detail = soup.find_all('div',class_ = 'trl-item1')
+
+d = []
+for item in detail:
+    d.append(item.div.text)
+
+community = soup.find('a',id =  'agantesfxq_C03_05').text
+
+district  = soup.find('a',id =  'agantesfxq_C03_07').text
+
+subdistrict  = soup.find('a',id =  'agantesfxq_C03_08').text
+
+school  = soup.find('a',id =  'agantesfxq_C03_09').text
+                   
+agent  =  soup.find('a',id =  'agantesfxq_C04_02').text              
+
+phone  =  soup.find('span',id = 'mobilecode').text    
+
+                   
+house_detail = soup.find('div',class_ = 'cont clearfix qu_bianqu1').find_all('span')
+
+house_detail_q = []
+for item in house_detail:
+    house_detail_q.append(item.text)
+
+pic_detail = soup.find('div',class_= 'cont-sty1 clearfix').find_all('img')
+
+pic_detail_q = []
+for item in pic_detail:
+    alt = item.attrs['alt']
+    link = item.attrs['data-src']
+    pic_detail_q.append([alt,link])
+
+pic_url = 'http://img3n.soufunimg.com/viewimage/agents/2013_11/25/M02/01/67/wKgFk1KSxlCIdF2OAANgi8H1Q08AACMPgDdmTAAA2Cj814/600x450c.jpg'
+html = requests.get(pic_url,headers=headers)
+with open('picture.jpg', 'wb') as file:
+    file.write(html.content)
+    file.close()
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
